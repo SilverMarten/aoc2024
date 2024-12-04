32e8a2dfa0453a61fa4c05ec0c10905cbdfaedc3
Paul Cormier <lastloginname@gmail.com>
2024-12-04 12:09:02 -0500

    Add diagonal directions

--- src/main/java/aoc/Direction.java
+++ src/main/java/aoc/Direction.java
@@ -6,21 +6,34 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Direction {
+
     RIGHT('>', 'R', Coordinate.of(0, 1)),
+    RIGHT_DOWN('↘', 'C', Coordinate.of(1, 1)),
     DOWN('v', 'D', Coordinate.of(1, 0)),
+    DOWN_LEFT('↙', 'Z', Coordinate.of(1, -1)),
     LEFT('<', 'L', Coordinate.of(0, -1)),
-    UP('^', 'U', Coordinate.of(-1, 0));
+    LEFT_UP('↖', 'Q', Coordinate.of(-1, -1)),
+    UP('^', 'U', Coordinate.of(-1, 0)),
+    UP_RIGHT('↗', 'E', Coordinate.of(-1, 1)),
+    ;
+
+
 
     private final char symbol;
+
     private final char letter;
+
     private final Coordinate translation;
 
     private static final Map<Character, Direction> symbolMap = Collections.unmodifiableMap(Stream.of(Direction.values())
                                                                                                  .collect(Collectors.toMap(d -> d.symbol,
                                                                                                                            d -> d)));
+
     private static final Map<Character, Direction> letterMap = Collections.unmodifiableMap(Stream.of(Direction.values())
                                                                                                  .collect(Collectors.toMap(d -> d.letter,
                                                                                                                            d -> d)));
+
+
 
     private Direction(char symbol, char letter, Coordinate translation) {
         this.symbol = symbol;
@@ -28,40 +41,54 @@ public enum Direction {
         this.translation = translation;
     }
 
+
+
     public static Direction withSymbol(char symbol) {
         return symbolMap.get(symbol);
     }
+
+
 
     public static Direction withLetter(char letter) {
         return letterMap.get(letter);
     }
 
+
+
     public static Direction oppositeOf(Direction direction) {
-        switch (direction) {
-            case UP:
-                return DOWN;
-            case RIGHT:
-                return LEFT;
-            case DOWN:
-                return UP;
-            case LEFT:
-                return RIGHT;
-            default:
-                throw new IllegalArgumentException();
-        }
+        return switch (direction) {
+            case UP -> DOWN;
+            case UP_RIGHT -> DOWN_LEFT;
+            case RIGHT -> LEFT;
+            case RIGHT_DOWN -> LEFT_UP;
+            case DOWN -> UP;
+            case DOWN_LEFT -> UP_RIGHT;
+            case LEFT -> RIGHT;
+            case LEFT_UP -> RIGHT_DOWN;
+
+            default -> throw new IllegalArgumentException();
+        };
     }
+
+
 
     public Direction opposite() {
         return oppositeOf(this);
     }
 
+
+
     public char getSymbol() {
         return symbol;
     }
 
+
+
     public char getLetter() {
         return letter;
     }
+
+
 
     public Coordinate getTranslation() {
         return translation;
