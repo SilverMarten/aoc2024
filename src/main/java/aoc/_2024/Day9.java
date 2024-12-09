c2e1cae173cf379a3394ccf7fb2eb9b11e387255
Paul Cormier <lastloginname@gmail.com>
2024-12-09 07:59:35 -0500

    Solve Day 9 - part 1

---
+++ src/main/java/aoc/_2024/Day9.java
@@ -0,0 +1,132 @@
+package aoc._2024;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import org.slf4j.LoggerFactory;
+
+import aoc.FileUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2024/day/9
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day9 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day9.class);
+
+    private static final String INPUT_TXT = "input/Day9.txt";
+
+    private static final String TEST_INPUT_TXT = "testInput/Day9.txt";
+
+
+
+    public static void main(String[] args) {
+
+        var resultMessage = "The filesystem checksum is {}";
+
+        log.info("Part 1:");
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+
+        var expectedTestResult = 1928;
+        var testResult = part1(testLines.getFirst());
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info(resultMessage, part1(lines.getFirst()));
+
+        // PART 2
+        resultMessage = "{}";
+
+        log.info("Part 2:");
+        log.setLevel(Level.DEBUG);
+
+        expectedTestResult = 1_234_567_890;
+        testResult = part2(testLines.getFirst());
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        log.info(resultMessage, part2(lines.getFirst()));
+    }
+
+
+
+    /**
+     * Compact the amphipod's hard drive using the process he requested. What is
+     * the resulting filesystem checksum?
+     * 
+     * @param line The line read from the input.
+     * @return The value calculated for part 1.
+     */
+    private static long part1(final String line) {
+
+        final AtomicInteger index = new AtomicInteger(0);
+        final List<Integer> memory = new ArrayList<>();
+        final List<Integer> freeSpaces = new ArrayList<>();
+
+        // Sort the input into free spaces and memory contents
+        line.chars()
+            .map(c -> c - '0')
+            .forEach(m -> {
+                if (index.get() % 2 == 0) {
+                    IntStream.range(0, m).forEach(x -> memory.add(index.get() / 2));
+                } else {
+                    IntStream.range(0, m).forEach(x -> freeSpaces.add(memory.size() + freeSpaces.size()));
+                }
+                index.incrementAndGet();
+            });
+
+        log.debug("Memory: {}", memory);
+        log.debug("Free spaces: {}", freeSpaces);
+
+        // Fill the free spaces with the end of the memory content
+        index.set(0);
+        freeSpaces.stream()
+                  .filter(i -> i <= memory.size())
+                  .forEach(i -> memory.add(i, memory.removeLast()));
+
+        log.debug("Defragmented memory: {}", memory);
+
+        index.set(0);
+        return memory.stream().mapToLong(m -> m * index.getAndIncrement()).sum();
+
+    }
+
+
+
+    /**
+     * 
+     * @param line The line read from the input.
+     * @return The value calculated for part 2.
+     */
+    private static long part2(final String line) {
+
+        return -1;
+    }
+
+}
