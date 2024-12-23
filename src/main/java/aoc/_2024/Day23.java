4249d221e6e623b48e968c35b1d935d2ac009927
Paul Cormier <lastloginname@gmail.com>
2024-12-23 07:52:24 -0500

    WIP - Day 23 - part 1

---
+++ src/main/java/aoc/_2024/Day23.java
@@ -0,0 +1,161 @@
+package aoc._2024;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.LoggerFactory;
+
+import aoc.FileUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2024/day/23
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day23 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day23.class);
+
+    private static final String INPUT_TXT = "input/Day23.txt";
+
+    private static final String TEST_INPUT_TXT = "testInput/Day23.txt";
+
+
+
+    public static void main(String[] args) {
+
+        var resultMessage = "There are {} computer groups containing at least one computer who's name starts with 't'.";
+
+        log.info("Part 1:");
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+
+        var expectedTestResult = 7;
+        var testResult = part1(testLines);
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info(resultMessage, part1(lines));
+
+        // PART 2
+        resultMessage = "{}";
+
+        log.info("Part 2:");
+        log.setLevel(Level.DEBUG);
+
+        expectedTestResult = 1_234_567_890;
+        testResult = part2(testLines);
+
+        log.info("Should be {}", expectedTestResult);
+        log.info(resultMessage, testResult);
+
+        if (testResult != expectedTestResult)
+            log.error("The test result doesn't match the expected value.");
+
+        log.setLevel(Level.INFO);
+
+        log.info(resultMessage, part2(lines));
+    }
+
+
+
+    /**
+     * Find all the sets of three inter-connected computers. How many contain at
+     * least one computer with a name that starts with t?
+     * 
+     * @param lines The lines read from the input.
+     * @return The value calculated for part 1.
+     */
+    private static long part1(final List<String> lines) {
+        Map<String, Computer> computers = new HashMap<>();
+
+        lines.stream()
+             .map(l -> l.split("-"))
+             .forEach(c -> {
+                 var c1 = computers.computeIfAbsent(c[0], Computer::new);
+                 var c2 = computers.computeIfAbsent(c[1], Computer::new);
+                 c1.addNeighbour(c2);
+             });
+
+        var groups = computers.values()
+                              .stream()
+                              .flatMap(c1 -> c1.neighbours()
+                                               .stream()
+                                               .flatMap(c2 -> c2.neighbours()
+                                                                .stream()
+                                                                .filter(c3 -> c3.neighbours().contains(c1))
+                                                                .map(c3 -> List.of(c1, c2, c3))))
+                              .toList();
+
+        log.debug("Groups:\n{}", groups.stream().map(l -> l.stream()
+                                                          .map(Computer::name)
+                                                          .collect(Collectors.joining(",")))
+                                      .collect(Collectors.joining("\n")));
+
+        return groups.stream()
+                     .filter(l -> l.stream().map(Computer::name).anyMatch(n -> n.startsWith("t")))
+                     .count();
+    }
+
+
+
+    /**
+     * 
+     * @param lines The lines read from the input.
+     * @return The value calculated for part 2.
+     */
+    private static long part2(final List<String> lines) {
+
+        return -1;
+    }
+
+
+
+    private record Computer(String name, Set<Computer> neighbours) {
+
+        public Computer(String name) {
+            this(name, new HashSet<>());
+        }
+
+
+
+        public void addNeighbour(Computer neighbour) {
+            this.neighbours.add(neighbour);
+            neighbour.neighbours.add(this);
+        }
+
+
+
+        @Override
+        public final int hashCode() {
+            return Objects.hashCode(name);
+        }
+
+
+
+        @Override
+        public final String toString() {
+            return String.format("%s (%s)", name, neighbours.stream().map(Computer::name).collect(Collectors.joining(", ")));
+        }
+    }
+}
