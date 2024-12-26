2f76158a3eec177fba02cc099d55689b9ae9dadf
Paul Cormier <lastloginname@gmail.com>
2024-12-25 22:27:18 -0500

    Day 24 - part 2
    Tried to manually identify the swaps, but failed.

--- src/main/java/aoc/_2024/Day24.java
+++ src/main/java/aoc/_2024/Day24.java
@@ -11,9 +11,12 @@ import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
@@ -38,7 +41,11 @@ public class Day24 {
 
     private static final String INPUT_TXT = "input/Day24.txt";
 
+    private static final String INPUT_2_TXT = "input/Day24-2.txt";
+
     private static final String TEST_INPUT_TXT = "testInput/Day24.txt";
+
+    private static final String TEST_INPUT_2_TXT = "testInput/Day24-2.txt";
 
 
 
@@ -74,8 +81,9 @@ public class Day24 {
         log.info("Part 2:");
         log.setLevel(Level.DEBUG);
 
+        List<String> testLines2 = FileUtils.readFile(TEST_INPUT_2_TXT);
         var expectedTestResult2 = "aaa,aoc,bbb,ccc,eee,ooo,z24,z99";
-        var testResult2 = part2(testLines);
+        var testResult2 = part2_example(testLines2);
 
         log.info("Should be {}", expectedTestResult2);
         log.info(resultMessage, testResult2);
@@ -83,9 +91,10 @@ public class Day24 {
         if (!expectedTestResult2.equals(testResult2))
             log.error("The test result doesn't match the expected value.");
 
-        log.setLevel(Level.INFO);
+        //        log.setLevel(Level.INFO);
+        List<String> lines2 = FileUtils.readFile(INPUT_2_TXT);
 
-        log.info(resultMessage, part2(lines));
+        log.info(resultMessage, part2(lines2));
     }
 
 
@@ -167,6 +176,119 @@ public class Day24 {
      * @param lines The lines read from the input.
      * @return The value calculated for part 2.
      */
+    private static String part2_example(final List<String> lines) {
+
+        Map<String, Boolean> values = new HashMap<>();
+
+        // Parse the inputs
+        lines.stream()
+             .filter(l -> l.contains(": "))
+             .map(l -> l.split(": "))
+             .forEach(v -> values.put(v[0], "1".equals(v[1])));
+
+        var functions = new DualHashBidiMap<>(Map.<String, BiFunction<Boolean, Boolean, Boolean>>of("AND", (a, b) -> a && b,
+                                                                                                    "OR", (a, b) -> a || b,
+                                                                                                    "XOR", (a, b) -> a ^ b));
+
+        // Parse the gates
+        var gates = lines.stream()
+                         .filter(l -> l.contains("->"))
+                         .map(l -> l.split(" "))
+                         .map(l -> new Gate(l[0], l[2], l[4], functions.get(l[1])))
+                         .toList();
+
+        Map<String, String> gateTypes = new HashMap<>();
+        gates.forEach(g -> gateTypes.put(g.result, functions.getKey(g.operation())));
+
+        /*Graph<String, DefaultEdge> graph = new SimpleDirectedGraph<>(DefaultEdge.class);
+        
+        gates.forEach(g -> {
+            graph.addVertex(g.input1());
+            graph.addVertex(g.input2());
+            graph.addVertex(g.result());
+            graph.addEdge(g.input1(), g.result());
+            graph.addEdge(g.input2(), g.result());
+        });
+        var exporter = new GmlExporter<String, DefaultEdge>();
+        exporter.setVertexAttributeProvider(v -> Map.of("label",
+                                                        DefaultAttribute.createAttribute(ObjectUtils.defaultIfNull(gateTypes.get(v), "") +
+                                                                                         "\n" + v)));
+        exporter.setParameter(GmlExporter.Parameter.EXPORT_VERTEX_LABELS, true);
+        
+        try (var fileOut = new FileWriter("Day 24 - part 2" + (log.isDebugEnabled() ? " - example" : "") + ".gml")) {
+            exporter.exportGraph(graph, fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        // The circuit is supposed to be an adder. See which outputs show up wrong given one bit at a time.
+        var inputBits = (int) values.keySet().stream().filter(k -> k.startsWith("x")).count();
+        IntStream.range(0, inputBits).forEach(i -> {
+            values.put(String.format("x%02d", i), false);
+            values.put(String.format("y%02d", i), false);
+        });
+
+        IntStream.range(0, inputBits).forEach(i -> {
+            if (i > 0) {
+                values.put(String.format("x%02d", i - 1), false);
+                values.put(String.format("y%02d", i - 1), false);
+            }
+            values.put(String.format("x%02d", i), true);
+            values.put(String.format("y%02d", i), true);
+
+            var outputValues = runCircuit(values, gates);
+
+            var xValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("x"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+            var yValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("y"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+            var zValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("z"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+
+            log.debug("X {} ({}) & Y {} ({}) = Z {} ({})",
+                      xValues, Long.parseLong(xValues, 2),
+                      yValues, Long.parseLong(yValues, 2),
+                      zValues, Long.parseLong(zValues, 2));
+            if ((Long.parseLong(xValues, 2) & Long.parseLong(yValues, 2)) != Long.parseLong(zValues, 2)) {
+                var zTrue = outputValues.entrySet()
+                                        .stream()
+                                        .filter(e -> e.getKey().startsWith("z") && e.getValue())
+                                        .map(Entry::getKey)
+                                        .toList();
+                log.warn(String.format("Output z%02d might need to swap with %s", i, zTrue.toString()));
+            }
+
+        });
+        return "";
+    }
+
+
+
+    /**
+     * Your system of gates and wires has four pairs of gates which need their
+     * output wires swapped - eight wires in total. Determine which four pairs
+     * of gates need their outputs swapped so that your system correctly
+     * performs addition; what do you get if you sort the names of the eight
+     * wires involved in a swap and then join those names with commas?
+     * 
+     * @param lines The lines read from the input.
+     * @return The value calculated for part 2.
+     */
     private static String part2(final List<String> lines) {
 
         Map<String, Boolean> values = new HashMap<>();
@@ -191,8 +313,8 @@ public class Day24 {
         Map<String, String> gateTypes = new HashMap<>();
         gates.forEach(g -> gateTypes.put(g.result, functions.getKey(g.operation())));
 
-        Graph<String, DefaultEdge> graph = new SimpleDirectedGraph<>(DefaultEdge.class);
-
+        /*Graph<String, DefaultEdge> graph = new SimpleDirectedGraph<>(DefaultEdge.class);
+        
         gates.forEach(g -> {
             graph.addVertex(g.input1());
             graph.addVertex(g.input2());
@@ -200,20 +322,139 @@ public class Day24 {
             graph.addEdge(g.input1(), g.result());
             graph.addEdge(g.input2(), g.result());
         });
-
         var exporter = new GmlExporter<String, DefaultEdge>();
         exporter.setVertexAttributeProvider(v -> Map.of("label",
                                                         DefaultAttribute.createAttribute(ObjectUtils.defaultIfNull(gateTypes.get(v), "") +
                                                                                          "\n" + v)));
         exporter.setParameter(GmlExporter.Parameter.EXPORT_VERTEX_LABELS, true);
-
+        
         try (var fileOut = new FileWriter("Day 24 - part 2" + (log.isDebugEnabled() ? " - example" : "") + ".gml")) {
             exporter.exportGraph(graph, fileOut);
         } catch (IOException e) {
             e.printStackTrace();
+        }*/
+
+        // The circuit is supposed to be an adder. See which outputs show up wrong given one bit at a time.
+        var inputBits = (int) values.keySet().stream().filter(k -> k.startsWith("x")).count();
+        IntStream.range(0, inputBits).forEach(i -> {
+            values.put(String.format("x%02d", i), false);
+            values.put(String.format("y%02d", i), false);
+        });
+
+        IntStream.range(0, inputBits).forEach(i -> {
+            if (i > 0) {
+                values.put(String.format("x%02d", i - 1), false);
+                values.put(String.format("y%02d", i - 1), false);
+            }
+            values.put(String.format("x%02d", i), true);
+            values.put(String.format("y%02d", i), true);
+
+            var outputValues = runCircuit(values, gates);
+
+            var xValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("x"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+            var yValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("y"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+            var zValues = outputValues.entrySet()
+                                      .stream()
+                                      .filter(e -> e.getKey().startsWith("z"))
+                                      .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                      .map(Entry::getValue)
+                                      .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                      .collect(Collectors.joining());
+
+            log.debug("X {} ({}) + Y {} ({}) = Z {} ({})",
+                      xValues, Long.parseLong(xValues, 2),
+                      yValues, Long.parseLong(yValues, 2),
+                      zValues, Long.parseLong(zValues, 2));
+            if (Long.parseLong(xValues, 2) + Long.parseLong(yValues, 2) != Long.parseLong(zValues, 2)) {
+                var zTrue = outputValues.entrySet()
+                                        .stream()
+                                        .filter(e -> e.getKey().startsWith("z") && e.getValue())
+                                        .map(Entry::getKey)
+                                        .toList();
+                log.warn(String.format("Output z%02d might need to swap with %s", i + 1, zTrue.toString()));
+            }
+
+        });
+
+        IntStream.range(0, 100)
+                 .forEach(j -> {
+                     long x = RandomUtils.insecure().randomLong();
+                     long y = RandomUtils.insecure().randomLong();
+
+                     IntStream.range(0, inputBits).forEach(i -> {
+                         values.put(String.format("x%02d", i), (x & (int) Math.pow(2, i)) != 0);
+                         values.put(String.format("y%02d", i), (y & (int) Math.pow(2, i)) != 0);
+                     });
+
+                     var outputValues = runCircuit(values, gates);
+
+                     var xValues = outputValues.entrySet()
+                                               .stream()
+                                               .filter(e -> e.getKey().startsWith("x"))
+                                               .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                               .map(Entry::getValue)
+                                               .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                               .collect(Collectors.joining());
+                     var yValues = outputValues.entrySet()
+                                               .stream()
+                                               .filter(e -> e.getKey().startsWith("y"))
+                                               .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                               .map(Entry::getValue)
+                                               .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                               .collect(Collectors.joining());
+                     var zValues = outputValues.entrySet()
+                                               .stream()
+                                               .filter(e -> e.getKey().startsWith("z"))
+                                               .sorted(Comparator.comparing(Entry<String, Boolean>::getKey).reversed())
+                                               .map(Entry::getValue)
+                                               .map(b -> Boolean.TRUE.equals(b) ? "1" : "0")
+                                               .collect(Collectors.joining());
+
+                     log.debug("X {} ({}) + Y {} ({}) = Z {} ({})",
+                               xValues, Long.parseLong(xValues, 2),
+                               yValues, Long.parseLong(yValues, 2),
+                               zValues, Long.parseLong(zValues, 2));
+                     if (Long.parseLong(xValues, 2) + Long.parseLong(yValues, 2) != Long.parseLong(zValues, 2)) {
+                         log.error("Adder is not working!");
+                     }
+                 });
+
+        return Stream.of("z15", "z16", "z23", "z24", "nbc", "svm", "z39", "fnr").sorted().collect(Collectors.joining(","));
+    }
+
+
+
+    private static Map<String, Boolean> runCircuit(Map<String, Boolean> values, List<Gate> gates) {
+
+        Map<String, Boolean> localValues = new HashMap<>(values);
+        Queue<Gate> gatesToCheck = new ArrayDeque<>(gates);
+        while (!gatesToCheck.isEmpty()) {
+            var nextGate = gatesToCheck.poll();
+
+            if (localValues.containsKey(nextGate.input1()) &&
+                localValues.containsKey(nextGate.input2()) &&
+                !localValues.containsKey(nextGate.result())) {
+                localValues.put(nextGate.result(), nextGate.operation()
+                                                           .apply(localValues.get(nextGate.input1()), localValues.get(nextGate.input2())));
+            } else {
+                // Re-queue
+                gatesToCheck.add(nextGate);
+            }
         }
 
-        return "";
+        return localValues;
     }
 
 
